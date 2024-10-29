0b8589fda225fe5ac5dca620056662c1ed3096e9
awilh37 <97398812+awilh37@users.noreply.github.com>
2024-10-29 15:46:52 +0000

    Co-authored-by: blungi-schoolacct <blungi-schoolacct@users.noreply.github.com>

--- prime.py
+++ prime.py
@@ -1,11 +1,37 @@
+# modlules
 from time import sleep
-i=2
-t=136279841
-while t!=0:
-    i=i*2
-    t=t-1
-    print(t)
-    print(i)
-    sleep(.01)
-i=i-1
-print(i)
+
+# global declarations
+global n
+global d
+global q
+global primes
+global notPrimes
+
+# variables
+n=1
+d=2
+q=1
+primes=[1]
+notPrimes=[]
+
+# functions
+def prime():
+    n=+1
+    d=1
+    q=n/d
+    while True:
+        d=+1
+        q=n/d
+        if q>n/2:
+            print(str(n),"is prime")
+            primes.append(n)
+            break
+        if not(str(q).__contains__(".")):
+            print(str(n),"isn't prime")
+            notPrimes.append(n)
+            break
+
+# forever loop
+while True:
+    prime()
